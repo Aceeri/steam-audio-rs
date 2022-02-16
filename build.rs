3d108351b4e691b86c8249e290dd0c5904e82234
Aceeri <conmcclusk@gmail.com>
2022-02-16 01:21:18 -0800

    Fix up a bit, still needs work

--- build.rs
+++ build.rs
@@ -7,13 +7,16 @@ use std::path::Path;
 
 fn main() {
     for (var, value) in env::vars() {
-        eprintln!("{var}={value}", var=var, value=value);
+        //eprintln!("{var}={value}", var=var, value=value);
     }
 
     println!("cargo:rustc-link-lib=dylib=phonon");
+    println!("cargo:rerun-if-changed=build.rs");
 
     let builder = Builder::default()
         .header("headers/phonon.h")
+        .header("headers/phonon_version.h")
+        //.header("headers/phonon_interfaces.h")
         .rustified_enum("IPL(.*)");
 
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -21,6 +24,4 @@ fn main() {
 
     let bindings = builder.generate().unwrap();
     bindings.write_to_file(&dest_path).unwrap();
-
-    eprintln!("Generated Bindings => {:?}", dest_path);
 }
